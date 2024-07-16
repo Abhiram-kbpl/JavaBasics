f36eacf2963303267bcc0878a1d5dc61f2a382d6
abhiramt <abhiramdevt2024@gmail.com>
2024-07-16 13:28:44 +0530

    first

---
+++ datatype/src/App.java
@@ -0,0 +1,51 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+       //  boolean
+       //Stores true or false values
+        boolean a = true ;
+        System.out.println("boolian "+ a);
+
+
+        // string 
+        // stores string 
+        String b = "one" ;
+        System.out.println("string"+ b);
+
+        // intiger 
+        //Stores whole numbers from -2,147,483,648 to 2,147,483,647
+
+        int    c = 3 ;
+        System.out.println("intiger " + c);
+        
+        // long  
+        //Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        long z = 35; 
+        System.out.println("long"+z );
+
+        // float
+        // stores the fractional data of 6 to 7 decimals  
+        float d = 3.0f ;
+        System.out.println("float " + d);
+
+       
+         // double 
+          // stores the fractional data of 6 to 7 decimals
+        double  f = 3.4 ;
+        System.out.println("double " + f);
+
+         // character 
+        //stores a single chracter
+        char   e = 1 ;
+        System.out.println("character "+ e);
+
+        
+        
+
+
+
+
+
+       
+    }
+}
