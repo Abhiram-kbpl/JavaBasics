eabb71213715fadf245e7979de7a83178ef2e982
abhiramt <abhiramdevt2024@gmail.com>
2024-07-16 14:17:44 +0530

    operation

---
+++ JavaOperations/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args)  {
+        // assign values 
+        int a = 3;
+        int b = 4;
+        
+        // addition 
+        int c = a + b ;
+
+        //subctration
+        int d = a - b ;
+
+        //multiplication 
+        int e = a * b ;
+
+        //devision
+        int f = a / b ;
+        
+        //multiple operation
+         int g = (a+b)/(a-b) ;
+        
+        System.out.println(a + " " + b + " " + c +" "+ d + " " + f +" "+g );
+
+    }
+}
