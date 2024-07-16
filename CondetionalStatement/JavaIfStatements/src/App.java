4262c05fb07d1a44bfe12bc90b1c8c7080874f13
abhiramt <abhiramdevt2024@gmail.com>
2024-07-16 17:25:02 +0530

    if statement

--- CondetionalStatement/JavaIfStatements/src/App.java
+++ CondetionalStatement/JavaIfStatements/src/App.java
@@ -1,5 +1,24 @@
+
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter a number a : ");
+      int a =sc.nextInt();
+
+      System.out.println("enter a number b : ");
+      int b = sc.nextInt();
+
+      if (a<b) {
+        System.out.println("a is less than b ");
+         }
+
+        if (a>b) {
+            System.out.println("a is greater  than b ");
+        }
+
     }
 }
