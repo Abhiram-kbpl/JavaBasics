1943585c027ba6be89f50ddade47101f25579dee
abhiramt <abhiramdevt2024@gmail.com>
2024-07-16 14:02:25 +0530

    java variable

---
+++ JavaVariable/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args)  {
+        System.out.println("print next line");
+        System.out.print("print ");
+        System.out.print("print same line ");
+        System.out.println("\nfirstline\nsecond line\nthird line");
+
+    }
+}
