1805e39333d65fccda2a797e55b7b40dd25e640b
abhiramt <abhiramdevt2024@gmail.com>
2024-07-17 16:24:17 +0530

    switch statement

--- CondetionalStatement/JavaSwitch/src/App.java
+++ CondetionalStatement/JavaSwitch/src/App.java
@@ -1,5 +1,46 @@
+
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
+        Scanner sc =new Scanner(System.in);
+        System.out.println("PRESS THE NUMBER BETWEEN 1 TO 5 !! ");
+
+        int number = sc.nextInt();
+        switch (number) {
+            case 1:
+               {
+                System.out.println("u entered 1");
+               }
+            
+            break;
+
+            case 2:
+            {
+                System.out.println("u entered 2");
+               }
+            break;
+            case 3:
+            {
+                System.out.println("u entered 3");
+               }
+             break;
+             case 4:
+             {
+                System.out.println("u entered 4");
+               }
+            break;
+
+            case 5:
+            {
+                System.out.println("u entered 5");
+               }
+            break;
+
+            default:System.out.println("enter a number between 1 and 5");
+             sc.close();
+        }
+    
     }
 }
