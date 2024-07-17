91a2e855db365a7d5c55e36df0961fade5235de7
abhiramt <abhiramdevt2024@gmail.com>
2024-07-17 15:33:27 +0530

    new

--- CondetionalStatement/JavaElseIF/src/App.java
+++ CondetionalStatement/JavaElseIF/src/App.java
@@ -1,5 +1,23 @@
+
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the first number : ");
+        int first = sc.nextInt();
+
+        System.out.println("enter the second number : "); 
+        int second = sc.nextInt();
+
+        if(first>second)
+             {System.out.println("first number is larger ");}
+
+        else
+             {System.out.println("second number is larger ");}
+
+      sc.close();
     }
 }
