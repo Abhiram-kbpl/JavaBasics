22ff266bd807ec62ad4de69d1194d791a602985a
abhiramt <abhiramt2018@gmail.com>
2024-07-23 07:08:38 +0530

    while loop

--- JavaLoops/Javawhileloop/src/App.java
+++ JavaLoops/Javawhileloop/src/App.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int correctPassword  = 1234567;
+
+        System.out.print("enter the password to login : ");
+        int password =sc.nextInt();
+
+        while (password != correctPassword ) {
+            
+            System.out.println("password entered is wrong please try again  ");
+
+            System.out.print("enter the password to login : ");
+
+            password =sc.nextInt();
+        }
+            System.out.println("you entered the correct password  ");
+            
+            System.out.println("WELCOME");
+
+            sc.close();
+        
+
+        }
+
+
+
+        
     }
-}
+
