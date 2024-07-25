2348ffcba7a5772d3739c793329bd4bd2dccc318
abhiramt <abhiramt2018@gmail.com>
2024-07-25 09:01:29 +0530

    pattern

--- JavaPattern/src/App.java
+++ JavaPattern/src/App.java
@@ -1,11 +1,12 @@
-
 public class App {
     public static void main(String[] args) {
-        //print an solid rectangle 
-        for(int i =0; i<4;i++)
-        System.out.println();
-        {for(int j =0; j<4;j++){System.out.println("*");}
-        
-       
+        // Print a solid rectangle
+        for(int i = 0; i < 4; i++) {
+            System.out.println(); // Move to the next line after each row
+            for(int j = 0; j < 4; j++) {
+                System.out.print("*");
+            }
+            
+        }
     }
-}}
+}
