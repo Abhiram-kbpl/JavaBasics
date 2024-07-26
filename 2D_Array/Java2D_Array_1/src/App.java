5db9554c64a83ecd0fe1f32a8c8b6957f474060a
abhiramt <abhiramt2018@gmail.com>
2024-07-26 19:31:06 +0530

    2d array

--- 2D_Array/Java2D_Array_1/src/App.java
+++ 2D_Array/Java2D_Array_1/src/App.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args)  {
+        Scanner SC = new Scanner(System.in);
+
+        System.out.println("enter the no of rows ");
+        int row    = SC.nextInt();
+
+        System.out.println("enter the no of colums ");
+        int colums = SC.nextInt();
+
+        //take input of rows and colums 
+
+        int[][] number =new int[row][colums];
+
+        //input
+        
+        for(int i =0;i<row;i++){
+            for(int j = 0 ;j<colums;j++){
+             number[i][j]= SC.nextInt();
+            }
+       
+        }
+        //print the values 
+       
+        for(int i =0;i<row;i++){
+            for(int j = 0 ;j<colums;j++){
+             System.out.print(number[i][j]);
+            
+            }
+            System.out.println();
+        }
+        
+
+
+
+
+        
     }
 }
