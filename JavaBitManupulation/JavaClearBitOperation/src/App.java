b1391a06425909b30a24cbb0bdee9511d1876c57
abhiramt <abhiramdevt2024@gmail.com>
2024-08-02 15:49:37 +0530

    clear bit

--- JavaBitManupulation/JavaClearBitOperation/src/App.java
+++ JavaBitManupulation/JavaClearBitOperation/src/App.java
@@ -2,5 +2,10 @@ public class App {
     public static void main(String[] args)  {
        int n =5;
        int pos=2;
+       int bitmask=1<<pos;
+       int notbitmask=~(bitmask);
+       int clearbit=n&notbitmask;
+       System.out.println(clearbit);
+
     }
 }
