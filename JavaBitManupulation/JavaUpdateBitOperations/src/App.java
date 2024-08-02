c2ad3935d17162a3b12fc502d4272b1afe44e352
abhiramt <abhiramdevt2024@gmail.com>
2024-08-02 16:48:40 +0530

    bitupdate

--- JavaBitManupulation/JavaUpdateBitOperations/src/App.java
+++ JavaBitManupulation/JavaUpdateBitOperations/src/App.java
@@ -1,9 +1,28 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args)  {
-        
+      Scanner sc = new Scanner(System.in);
+
+      System.out.print("ENTER THE NO U WANT TO SET ");
+      int numb=sc.nextInt(); 
        int n =5;
        int pos=2;
        int bitmask=1<<pos;
+
+       if(numb==1){
+        int newnumb = n | bitmask ;
+        System.out.println(newnumb);
+
+       }else{
+        int newbitmsk=~(bitmask);
+        
+        int newnumb =newbitmsk & n;
+        System.out.println(newnumb);
+        
+       }
+
       
     }
 }
