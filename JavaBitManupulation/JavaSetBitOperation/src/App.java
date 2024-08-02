76aecb6190e4397e7c2100a6a32d7f19a771b096
abhiramt <abhiramdevt2024@gmail.com>
2024-08-02 15:42:40 +0530

    set bit

--- JavaBitManupulation/JavaSetBitOperation/src/App.java
+++ JavaBitManupulation/JavaSetBitOperation/src/App.java
@@ -1,5 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        public static void main(String[] args)  {
+            
+           int n =   5;
+           int pos = 2;
+
+           int bitmask =1<<3;
+           int setbit = bitmask | n ;
+           System.out.println(setbit);
+        
     }
+    
+        
 }
