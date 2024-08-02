9c2d9d19fac65737c100b259edcc40a8012b1e56
abhiramt <abhiramdevt2024@gmail.com>
2024-08-02 15:29:06 +0530

    getbit

--- JavaBitManupulation/GetBitOperation/src/App.java
+++ JavaBitManupulation/GetBitOperation/src/App.java
@@ -1,5 +1,16 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        //find the bit at 2nd postion 
+        int n = 5 ;
+        int pos =2 ;
+        int bitmask = 1<<pos ;
+
+        if((bitmask & n)==0){
+            System.out.println("the bit was zero ");
+        }
+        else{
+            System.out.println("the bit was one");
+        }
+        
     }
 }
