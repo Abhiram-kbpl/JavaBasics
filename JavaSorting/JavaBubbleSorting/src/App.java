eb6212ac7adda595b66e24aa0240479055689eb3
abhiramt <abhiramdevt2024@gmail.com>
2024-08-03 12:38:39 +0530

    sort

--- JavaSorting/JavaBubbleSorting/src/App.java
+++ JavaSorting/JavaBubbleSorting/src/App.java
@@ -15,7 +15,7 @@ public class App {
                 }
             }
         }  
-
+            //print array
         for(int i =0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
